220548cf98461a40df0106eb24e403d1c04c21cc
Pon-Rahul <ponrahul55@gmail.com>
2023-09-29 11:57:26 +0530

    app,index

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Home from './Homepage/Home';
+import Watchlist from './Watchlist/Watchlist';
+import { Routes, Route } from 'react-router-dom';
+
 
 function App() {
+
+  const [arr, setArr] = useState([])
+
+  const buttonHandler = (i) => {
+    const addarr = JSON.parse(localStorage.getItem("watchArr")) || []
+    addarr.push(arr[i])
+    localStorage.setItem("watchArr", JSON.stringify(addarr));
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='' element={<Home setArr={setArr}
+        arr={arr}
+        add={buttonHandler} />}></Route>
+      <Route path='watchlist' element={<Watchlist
+
+      />}></Route>
+    </Routes>
   );
 }
 
